098736a77c58824c5a157cdb5ee4a71ca1078259
Rune <rune.dedonder@gmail.com>
2023-05-08 21:31:47 +0200

    add service essentials

---
+++ service.tf
@@ -0,0 +1,20 @@
+resource "google_cloud_run_service" "cloud_run_service" {
+  name     = var.cloud_run_service_name
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.artifact_registry_location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.artifact_registry.repository_id}/${var.cloud_run_service_image_name}:latest"
+      }
+      service_account_name = var.cloud_run_service_account
+      vpc_connector {
+        name = var.vpc_access_connector
+      }
+      network {
+        name = google_compute_network.vpc_network.name
+        subnetwork_name = google_compute_subnetwork.vpc_subnet.name
+      }
+    }
+  }
+}
