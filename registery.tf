1640d5430abdda35bacf1bc9fc5e81beb77c8e57
Rune <rune.dedonder@gmail.com>
2023-05-08 21:33:49 +0200

    fix registery resource name

--- registery.tf
+++ registery.tf
@@ -1,4 +1,4 @@
-resource "google_artifact_registry_repository" "artifact_registryo" {
+resource "google_artifact_registry_repository" "artifact_registry" {
   location      = var.region
   repository_id = var.artifact_registry_name
   format        = "DOCKER"
