f27250352d7900f53f21aa87ea8e477132307399
Rune <rune.dedonder@gmail.com>
2023-05-09 21:17:15 +0200

    add configurable container version on service

--- service.tf
+++ service.tf
@@ -5,14 +5,14 @@ resource "google_cloud_run_service" "cloud_run_service" {
   template {
     spec {
       containers {
-        image = "${var.artifact_registry_location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.artifact_registry.repository_id}/${var.cloud_run_service_image_name}:latest"
+        image = "${var.artifact_registry_location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.artifact_registry.repository_id}/${var.cloud_run_service_image_name}:${var.cloud_run_service_container_version}"
       }
       service_account_name = var.cloud_run_service_account
       vpc_connector {
         name = var.vpc_access_connector
       }
       network {
-        name = google_compute_network.vpc_network.name
+        name            = google_compute_network.vpc_network.name
         subnetwork_name = google_compute_subnetwork.vpc_subnet.name
       }
     }
