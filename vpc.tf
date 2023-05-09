ffb6f2ca8a7f76fbfd205be49d6b87e81687c7ed
Rune <rune.dedonder@gmail.com>
2023-05-09 20:39:07 +0200

    add vpc access connector config

--- vpc.tf
+++ vpc.tf
@@ -3,9 +3,18 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "vpc_subnet" {
+resource "google_compute_subnetwork" "vpc_access_connector_subnet" {
   name          = var.vpc_subnet_name
+  region        = var.region
   ip_cidr_range = var.vpc_subnet_ip_cidr_range
   network       = google_compute_network.vpc_network.self_link
-  region        = var.region
+}
+
+resource "google_vpc_access_connector" "vpc_access_connector" {
+  name         = var.vpc_connector_name
+  region       = var.region
+  machine_type = var.vpc_machine_type
+  subnet { 
+    name = google_compute_subnetwork.vpc_access_connector_subnet.name 
+  }
 }
