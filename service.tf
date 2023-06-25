4162c99ad31f9784308417c97c710ef93c589402
Rune <rune.dedonder@gmail.com>
2023-06-25 19:39:28 +0200

    add service code

--- service.tf
+++ service.tf
@@ -1,47 +1,66 @@
 resource "google_cloud_run_service" "cloud_run_service" {
   name     = var.cloud_run_service_name
   location = var.region
+  project  = var.project_id
+
+  metadata {
+    namespace = var.project_id
+    annotations = {
+      "run.googleapis.com/ingress" = var.cloud_run_vpc_access_ingress
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
 
   template {
     spec {
-      # container_concurrency = 
-      # timeout_seconds = 
-      service_account_name = var.cloud_run_service_account
+      container_concurrency = var.cloud_run_container_concurrency
+      timeout_seconds       = var.cloud_run_timeout_seconds
+      service_account_name  = var.cloud_run_service_account
+
       containers {
-        image = "${var.artifact_registry_location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.artifact_registry.repository_id}/${var.cloud_run_service_image_name}:${var.cloud_run_service_container_version}"
-        # resources {
-        #   requests {
-        #     cpu = 
-        #     memory =
-        #   }
-        #   limits {
-        #     cpu = 
-        #     memory =
-        #   }
-        # }
-        # ports {
-        #   container_port = 
-        # }
+
+        image = var.cloud_run_service_image_location
+
+        resources {
+          requests = {
+            cpu    = var.cloud_run_cpu_request
+            memory = var.cloud_run_memory_request
+          }
+          limits = {
+            cpu    = var.cloud_run_cpu_limit
+            memory = var.cloud_run_memory_limit
+          }
+        }
+        ports {
+          container_port = var.cloud_run_container_port
+        }
+      }
+    }
+
+    metadata {
+      labels = {
+        "environment" : terraform.workspace
+        "run.googleapis.com/startupProbeType" = "Default"
+      }
+      annotations = {
+        # Scaling behaviour
+        "autoscaling.knative.dev/maxScale" = var.cloud_run_max_scale
+        "autoscaling.knative.dev/minScale" = var.cloud_run_min_scale
+        # Serverless VPC access connector
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.vpc_access_connector.name
+        "run.googleapis.com/vpc-access-egress"    = var.cloud_run_vpc_access_egress
+        # Set to true to enable CPU allocation only during request processing
+        "run.googleapis.com/cpu-throttling" = var.cloud_run_cpu_throttling
+        # Set to true to enable requests from a client to be directed to the same container
+        "run.googleapis.com/sessionAffinity" = var.cloud_run_session_affinity
+        # Set to true to enable CPU boosts to reduce startup time
+        "run.googleapis.com/startup-cpu-boost" = var.cloud_run_cpu_boost
       }
     }
   }
-  metadata {
-    labels = {
-      "environment" : terraform.workspace
-    }
-    annotations = {
-      # Scaling behaviour
-      # "autoscaling.knative.dev/maxScale" = 
-      # "autoscaling.knative.dev/minScale" = 
-      # Serverless VPC access connector
-      "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.vpc_access_connector.name
-      # "run.googleapis.com/vpc-access-egress" = 
-      # # Set to true to enable CPU allocation only during request processing
-      # "run.googleapis.com/cpu-throttling" = 
-      # # Set to true to enable requests from a client to be directed to the same container
-      # "run.googleapis.com/sessionAffinity" = 
-      # # Set to true to enable CPU boosts to reduce startup time
-      # "run.googleapis.com/startup-cpu-boost" = 
-    }
-  }
+  autogenerate_revision_name = true
 }
